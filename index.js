dcd7e6dbfc195c650c2f74784c067cda39149418
Delv34 <enderfeed2002@gmail.com>
2024-06-22 20:30:51 +1000

    updated

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/', authRouter)
-app.use('/', productRouter)
+app.use('/api', authRouter)
+app.use('/api', productRouter)
 
 const start = async ()=> {
     try {
